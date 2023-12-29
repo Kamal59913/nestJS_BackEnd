cf4cffd45c07011684085c71c115f660edc0d6d3
kamal <kamallochanboruah7@gmail.com>
2023-12-29 12:52:36 +0530

    new commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
 
   const allowedOrigins = ['http://localhost:3000', 'https://nest-js-front-end.vercel.app'];
 
